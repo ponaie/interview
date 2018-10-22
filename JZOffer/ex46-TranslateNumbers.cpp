9f1f2b5d86991db54fd6bb07c932bbd201d68659
ponaie <xxueqiang16@gmail.com>
2018-10-22 22:51:24 +0800

    Add JZOffer No.46 Translate Numbers

---
+++ JZOffer/ex46-TranslateNumbers.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************
+    > File Name: JZOffer/ex46-TranslateNumbers.cpp
+    > Author: Stranger
+    > Mail: xxueqiang16@gmail.com
+    > Created Time: Mon 22 Oct 2018 10:07:30 AM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int getTranslationCount(const string &number) {
+  int length = number.length();
+  int *counts = new int[length];
+  int count = 0;
+
+  for(int i=length-1; i>=0; --i) {
+    count = 0;
+    if(i<length-1) count = counts[i+1];
+    else count = 1;
+    if(i<length-1) {
+      int digit1 = number[i] - '0';
+      int digit2 = number[i] - '0';
+      int converted = digit1 * 10 + digit2;
+      if(converted >= 10 && converted <= 25) {
+        if(i<length-2) count += counts[i+2];
+        else count += 1;
+      }
+    }
+    counts[i] = count;
+  }
+
+  count = counts[0];
+  delete [] counts;
+
+  return count;
+}
+
+int getTranslationCount(int number) {
+  if(number<0) return 0;
+
+  string numberInString = to_string(number);
+  return getTranslationCount(numberInString);
+}
+
+int main() {}
