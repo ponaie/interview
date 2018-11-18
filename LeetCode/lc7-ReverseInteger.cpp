8ba7ec41be4314952d03b7fccde2c102f10ccd0e
ponaie <xxueqiang16@gmail.com>
2018-11-18 09:20:53 +0800

    Update LeetCode No.7 Reverse Integer

--- LeetCode/lc7-ReverseInteger.cpp
+++ LeetCode/lc7-ReverseInteger.cpp
@@ -5,25 +5,42 @@
     > Created Time: Fri 16 Nov 2018 11:57:27 PM CST
  ************************************************************************/
 
+#include <climits>
 #include <iostream>
 using namespace std;
 
+// To check for overflow/underflow, we could check if ret > 214748364 or
+// ret < –214748364 before multiplying by 10. On the other hand, we do
+// not need to check if ret == 214748364, why?
 int reverse(int x) {
-  long long res = 0;
-  bool isPositive = true;
-  if (x < 0) {
-    isPositive = false;
-    x *= -1;
-  }
-  while (x > 0) {
+  int res = 0;
+  while (x != 0) {
+    if (abs(res) > INT_MAX / 10) return 0;
     res = res * 10 + x % 10;
     x /= 10;
   }
-  if (res > INT_MAX) return 0;
-  if (isPositive)
-    return res;
-  else
-    return -res;
+  return res;
 }
 
-int main() {}
+// int reverse(int x) {
+//   long long res = 0;
+//   bool isPositive = true;
+//   if (x < 0) {
+//     isPositive = false;
+//     x *= -1;
+//   }
+//   while (x > 0) {
+//     res = res * 10 + x % 10;
+//     x /= 10;
+//   }
+//   if (res > INT_MAX) return 0;
+//   if (isPositive)
+//     return res;
+//   else
+//     return -res;
+// }
+
+int main() {
+  int result = reverse(INT_MIN);
+  cout << result << endl;
+}
