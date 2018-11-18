0df66eefd09f8eab2055a8e0c17975637bd205cb
ponaie <xxueqiang16@gmail.com>
2018-11-18 09:33:35 +0800

    Update LeetCode No.7 Reverse Integer

--- LeetCode/lc7-ReverseInteger.cpp
+++ LeetCode/lc7-ReverseInteger.cpp
@@ -24,6 +24,15 @@ int reverse(int x) {
 
 // int reverse(int x) {
 //   long long res = 0;
+//   while (x != 0) {
+//     res = 10 * res + x % 10;
+//     x /= 10;
+//   }
+//   return (res > INT_MAX || res < INT_MIN) ? 0 : res;
+// }
+
+// int reverse(int x) {
+//   long long res = 0;
 //   bool isPositive = true;
 //   if (x < 0) {
 //     isPositive = false;
