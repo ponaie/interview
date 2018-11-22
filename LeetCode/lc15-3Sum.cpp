722c88afa9ab5dff95ba3ea134277bf790993041
ponaie <xxueqiang16@gmail.com>
2018-11-22 17:48:32 +0800

    Add LeetCode No.15 3Sum

---
+++ LeetCode/lc15-3Sum.cpp
@@ -0,0 +1,41 @@
+/*************************************************************************
+    > File Name: LeetCode/lc15-3Sum.cpp
+    > Author: Stranger
+    > Mail: xxueqiang16@gmail.com
+    > Created Time: Thu 22 Nov 2018 05:47:00 PM CST
+ ************************************************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+  vector<vector<int>> res;
+  sort(nums.begin(), nums.end());
+  if (nums.empty() || nums.back() < 0 || nums.front() > 0) return res;
+
+  for (int k = 0; k < nums.size(); ++k) {
+    if (nums[k] > 0) break;
+    if (k > 0 && nums[k] == nums[k - 1]) continue;
+
+    int target = -nums[k];
+    int i = k + 1, j = nums.size() - 1;
+    while (i < j) {
+      if (nums[i] + nums[j] == target) {
+        res.push_back({nums[k], nums[i], nums[j]});
+        while (i < j && nums[i] == nums[i + 1]) ++i;
+        while (i < j && nums[j] == nums[j - 1]) --j;
+        ++i;
+        --j;
+
+      } else if (nums[i] + nums[j] < target)
+        ++i;
+      else
+        --j;
+    }
+  }
+  return res;
+}
+
+int main() {}
