47ad01457aa714e7d419532fa321121d70808cbc
ponaie <xxueqiang16@gmail.com>
2018-11-24 23:48:57 +0800

    Add LeetCode No.16 3Sum Closet

---
+++ LeetCode/lc16-3SumCloset.cpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+    > File Name: LeetCode/lc16-3SumCloset.cpp
+    > Author: Stranger
+    > Mail: xxueqiang16@gmail.com
+    > Created Time: Sat 24 Nov 2018 11:47:33 PM CST
+ ************************************************************************/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int threeSumClosest(vector<int>& nums, int target) {
+  int closest = nums[0] + nums[1] + nums[2];
+  int diff = abs(closest - target);
+  sort(nums.begin(), nums.end());
+  for (int i = 0; i < nums.size() - 2; ++i) {
+    int left = i + 1, right = nums.size() - 1;
+    while (left < right) {
+      int sum = nums[i] + nums[left] + nums[right];
+      int newDiff = abs(sum - target);
+      if (diff > newDiff) {
+        diff = newDiff;
+        closest = sum;
+      }
+      if (sum < target)
+        ++left;
+      else
+        --right;
+    }
+  }
+  return closest;
+}
+
+int main() {}
