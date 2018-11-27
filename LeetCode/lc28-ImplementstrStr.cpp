6e44253c499567c31c9fa1737348f911bb36fd89
ponaie <xxueqiang16@gmail.com>
2018-11-27 17:28:43 +0800

    Update LeetCode No.28 Implement strStr()

--- LeetCode/lc28-ImplementstrStr.cpp
+++ LeetCode/lc28-ImplementstrStr.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int strStr(string haystack, string needle) {
-    if (needle.empty()) return 0;
-    int m = haystack.size(), n = needle.size();
-    if (m < n) return -1;
-    for (int i = 0; i <= m - n; ++i) {
-        int j = 0;
-        for (j = 0; j < n; ++j) {
-            if (haystack[i + j] != needle[j]) break;
-        }
-        if (j == n) return i;
+  if (needle.empty()) return 0;
+  int m = haystack.size(), n = needle.size();
+  if (m < n) return -1;
+  for (int i = 0; i <= m - n; ++i) {
+    int j = 0;
+    for (j = 0; j < n; ++j) {
+      if (haystack[i + j] != needle[j]) break;
     }
-    return -1;
+    if (j == n) return i;
+  }
+  return -1;
 }
 
 int main() {}
