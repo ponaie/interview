faa1983ee21db036d1243554c84f3bd08fdc6a84
ponaie <xxueqiang16@gmail.com>
2018-12-01 19:58:51 +0800

    Add LeetCode No.445 Add Two Numbers II

---
+++ LeetCode/lc445-AddTwoNumberII.cpp
@@ -0,0 +1,48 @@
+/*************************************************************************
+    > File Name: LeetCode/lc445-AddTwoNumberII.cpp
+    > Author: Stranger
+    > Mail: xxueqiang16@gmail.com
+    > Created Time: Sat 01 Dec 2018 07:57:13 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+  stack<int> s1, s2;
+  while (l1) {
+    s1.push(l1->val);
+    l1 = l1->next;
+  }
+  while (l2) {
+    s2.push(l2->val);
+    l2 = l2->next;
+  }
+  int sum = 0;
+  ListNode* res = new ListNode(0);
+  while (!s1.empty() || !s2.empty()) {
+    if (!s1.empty()) {
+      sum += s1.top();
+      s1.pop();
+    }
+    if (!s2.empty()) {
+      sum += s2.top();
+      s2.pop();
+    }
+    res->val = sum % 10;
+    ListNode* head = new ListNode(sum / 10);
+    head->next = res;
+    res = head;
+    sum /= 10;
+  }
+  return res->val == 0 ? res->next : res;
+}
+
+int main() {}
