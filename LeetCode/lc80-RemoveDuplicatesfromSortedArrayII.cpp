63169ed5d3f79bd50b83bf5325c4c71924c75db2
ponaie <xxueqiang16@gmail.com>
2018-12-22 12:56:40 +0800

    Update LeetCode No.80 Remove Duplicates from Sorted Array II

--- LeetCode/lc80-RemoveDuplicatesfromSortedArrayII.cpp
+++ LeetCode/lc80-RemoveDuplicatesfromSortedArrayII.cpp
@@ -10,21 +10,32 @@
 using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
-  if (nums.empty()) return 0;
+  int n = nums.size();
+  if (n < 2) return n;
 
-  int pre = 0, cur = 1, count = 1;
-  while (cur < nums.size()) {
-    if (nums[pre] == nums[cur] && count == 0)
-      ++cur;
-    else {
-      if (nums[pre] == nums[cur])
-        --count;
-      else
-        count = 1;
-      nums[++pre] = nums[cur++];
-    }
-  }
-  return pre + 1;
+  int pos = 2;
+  for (int i = 2; i < n; ++i)
+    if (nums[i] != nums[pos - 2]) nums[pos++] = nums[i];
+
+  return pos;
 }
+
+// int removeDuplicates(vector<int>& nums) {
+//   if (nums.empty()) return 0;
+//
+//   int pre = 0, cur = 1, count = 1;
+//   while (cur < nums.size()) {
+//     if (nums[pre] == nums[cur] && count == 0)
+//       ++cur;
+//     else {
+//       if (nums[pre] == nums[cur])
+//         --count;
+//       else
+//         count = 1;
+//       nums[++pre] = nums[cur++];
+//     }
+//   }
+//   return pre + 1;
+// }
 
 int main() {}
