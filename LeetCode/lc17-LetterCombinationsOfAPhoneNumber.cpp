de32a11b713b1ae639f51e549eba48adabb71970
ponaie <xxueqiang16@gmail.com>
2018-12-26 22:36:38 +0800

    Add LeetCode No.17 Letter Combinations of a Phone Number

---
+++ LeetCode/lc17-LetterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+    > File Name: LeetCode/lc17-LetterCombinationsOfAPhoneNumber.cpp
+    > Author: Stranger
+    > Mail: xxueqiang16@gmail.com
+    > Created Time: Wed 26 Dec 2018 10:31:48 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void letterCombinationsDFS(string digits, string dict[], int level, string out,
+                           vector<string> &res) {
+  if (level == digits.size()) {
+    res.push_back(out);
+    return;
+  }
+  string str = dict[digits[level] - '0'];
+  for (int i = 0; i < str.size(); ++i) {
+    letterCombinationsDFS(digits, dict, level + 1, out + string(1, str[i]),
+                          res);
+  }
+}
+
+vector<string> letterCombinations(string digits) {
+  if (digits.empty()) return {};
+  vector<string> res;
+  string dict[] = {"",    "",    "abc",  "def", "ghi",
+                   "jkl", "mno", "pqrs", "tuv", "wxyz"};
+  letterCombinationsDFS(digits, dict, 0, "", res);
+  return res;
+}
+
+int main() {}
